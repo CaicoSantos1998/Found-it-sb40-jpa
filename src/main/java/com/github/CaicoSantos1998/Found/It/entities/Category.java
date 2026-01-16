f550ad7fce45d66fbf0a8d44037c3c59416dc5b8
CS <caico_fc@hotmail.com>
2026-01-16 12:34:28 -0300

    Association table created between product and category

--- src/main/java/com/github/CaicoSantos1998/Found/It/entities/Category.java
+++ src/main/java/com/github/CaicoSantos1998/Found/It/entities/Category.java
@@ -1,5 +1,6 @@
 package com.github.CaicoSantos1998.Found.It.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -17,8 +18,9 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-
-    @Transient
+    
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category() {
